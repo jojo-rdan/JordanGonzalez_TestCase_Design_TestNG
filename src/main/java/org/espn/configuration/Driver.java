70a5637117239636ae133a2600a67c140d7f5b01
jordan.gonzalez <jordan.olaves91@gmail.com>
2022-11-13 22:35:27 -0500

    first commit

---
+++ src/main/java/org/espn/configuration/Driver.java
@@ -0,0 +1,29 @@
+package org.espn.configuration;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Locale;
+
+public class Driver {
+    private WebDriver driver;
+
+    public Driver(String browser) {
+        switch (browser.toLowerCase(Locale.ROOT)) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+        }
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}

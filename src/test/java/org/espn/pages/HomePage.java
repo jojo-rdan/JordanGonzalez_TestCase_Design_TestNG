17a0727b7661bf0162ae260dc2d4474fa2c778ec
jordan.gonzalez <jordan.olaves91@gmail.com>
2022-11-15 20:15:55 -0500

    working on first test

--- src/test/java/org/espn/pages/HomePage.java
+++ src/test/java/org/espn/pages/HomePage.java
@@ -1,17 +1,24 @@
 package org.espn.pages;
 
+import org.espn.configuration.WebOperations;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage{
+    WebOperations webOperations = new WebOperations(getDriver());
     // Acá irán los elementos de la página con los que necesito interactuar
-    @FindBy(id = "global-user-trigger")
-    private WebElement hoverOpener;
-    @FindBy(xpath = "//body/div[@id='global-viewport']/div[3]/div[1]/ul[1]/li[7]/a[1]")
-    private WebElement logInOpener;
+    @FindBy(id= "global-user-trigger")
+    private WebElement userIcon;
 
+    @FindBy(id = "global-header > div.container > ul > li.user > div > div > ul.account-management > li:nth-child(7) > a")
+    private WebElement logInOption;
+
+    @FindBy(id = "oneid-iframe")
+    private WebElement iframe;
+    @FindBy(css = "#root > div:nth-child(3) > div > div > div:nth-child(1) > form")
+    private WebElement logInModal;
     @FindBy(id = "logo")
     private WebElement logo;
 
@@ -25,17 +32,33 @@ public class HomePage extends BasePage{
         super(driver);
     }
 
+
     // Métodos que harán lo que yo quiero
-    public void hoverOnLogIn() throws InterruptedException {
+    public void mouseOverUser() throws InterruptedException {
         Actions action = new Actions(getDriver());
-        action.moveToElement(hoverOpener).perform();
+        action.moveToElement(userIcon).perform();
         Thread.sleep(3000);
         getDriver().quit();
     }
-    public void clickOnLogInOpener(){
-        clickElement(logInOpener);
+    public void clickOnLogInHomePage(){
+        webOperations.clickElement(logInOption);
     }
-    public boolean isLoginDisplayed(){
-        return logInOpener.isDisplayed();
+    public void goToIframe() {
+        getDriver().switchTo().frame(iframe);
+    }
+    public boolean isLogInModalDisplayed(){
+        return logInModal.isDisplayed();
+    }
+    public boolean isLogoDisplayed(){
+        return logo.isDisplayed();
+    }
+    public boolean isLogInDisplayed(){
+        return logInButton.isDisplayed();
+    }
+    public boolean isSignUpDisplayed(){
+        return signUpButton.isDisplayed();
+    }
+    public void typeEmail(){
+
     }
 }
